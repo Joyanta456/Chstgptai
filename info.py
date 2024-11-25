8a4afa2e6889afa3f1088fdd5f03ea0fda8bd945
░▒▓█SOΠΔLI✿SΔHΔ█▓▒░ <124885646+OFFCode032@users.noreply.github.com>
2024-11-25 17:03:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,8 +6,7 @@ load_dotenv()
 
 API_ID = environ.get("API_ID", "19580422")
 API_HASH = environ.get("API_HASH", "0ea3c25451bfba7c5b69895e3a454463")
-BOT_TOKEN = environ.get(
-    "BOT_TOKEN", "6939162393:AAENT5n_SgOY1zVJQZjUgxEpt-yOdA2L5rE")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6939162393:AAENT5n_SgOY1zVJQZjUgxEpt-yOdA2L5rE")
 ADMIN = int(environ.get("ADMIN", "5953239864"))
 CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002220975470"))
 LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1001996681486")
